ea2dcca29316ec630b0c99c73ae74b45ff8de439
Bart van Irsel <bart@30.nl>
2011-02-10 10:31:32 +0100

    check for right module dir

--- _config.php
+++ _config.php
@@ -1,5 +1,9 @@
 <?php
 
+if (($SENDFRIEND_MODULE_DIR = basename(dirname(__FILE__))) != 'SendFriend') {
+	die("Send friend module should exist in the /SendFriend directory, not $SENDFRIEND_MODULE_DIR");
+}
+
 // directory which holds this module
 define('SENDFRIEND_DIR', 'SendFriend');
 
