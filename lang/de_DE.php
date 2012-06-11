a93f0a25921c2b3bd51fd9302a0fd39c17eeaa04
ivo <ivo.bathke@gmail.com>
2012-06-11 11:42:22 +0200

    german translation

---
+++ lang/de_DE.php
@@ -0,0 +1,23 @@
+<?php
+global $lang;
+
+$lang['de_DE']['SendFriend']['THANKYOU'] = 'Vielen Dank';
+$lang['de_DE']['SendFriend']['FORM_INTRO_TITLE'] = 'Sende diese Seite einem Freund.';
+$lang['de_DE']['SendFriend']['FORM_INTRO_TEXT'] = 'Bitte fülle das Formular aus.';
+$lang['de_DE']['SendFriend']['SENT_TEXT'] = 'Die Nachricht wurde versendet.';
+$lang['de_DE']['SendFriend']['BACKTO_PAGE'] = 'Zurück zur Seite.';
+$lang['de_DE']['SendFriend']['SENDFRIENDLINK'] = 'Seite empfehlen';
+
+$lang['de_DE']['SendFriend']['FORM_LABEL_YOURNAME'] = 'Ihr Name';
+$lang['de_DE']['SendFriend']['FORM_LABEL_YOUREMAIL'] = 'Ihre Email-Adresse';
+$lang['de_DE']['SendFriend']['FORM_LABEL_TONAME'] = 'Name des Empfänger';
+$lang['de_DE']['SendFriend']['FORM_LABEL_TOMAIL'] = 'Email des Empfänger';
+$lang['de_DE']['SendFriend']['FORM_LABEL_REMARKS'] = 'Bemerkungen';
+$lang['de_DE']['SendFriend']['FORM_LABEL_COPYSELF'] = 'Kopie an mich versenden';
+$lang['de_DE']['SendFriend']['FORM_LABEL_SENDBUTTON'] = 'Versenden';
+
+$lang['de_DE']['SendFriend']['SUBJECT'] = 'Interessante Seite auf ';
+$lang['de_DE']['SendFriend']['DEAR'] = 'Liebe(r) ';
+$lang['de_DE']['SendFriend']['MESSAGEFROM'] = 'Nachricht von ';
+$lang['de_DE']['SendFriend']['COPYOFMESSAGE'] = '---- Kopie der Nachricht von ';
+$lang['de_DE']['SendFriend']['TEXT'] = ' würde Dir gerne folgende Seite empfehlen: ';
